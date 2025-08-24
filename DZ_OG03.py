4f4224b47aec55b8ab3b1b097a368d9467a79654
g-pale <saburkin.s@outlook.com>
2025-08-24 13:17:14 +0300

    add square diagonal

--- DZ_OG03.py
+++ DZ_OG03.py
@@ -1,8 +1,9 @@
 def square(side):
-    'Функция выводит, периметр и площадь квадрата.'
+    'Функция выводит, периметр, площадь и длину диагонали квадрата.'
     perimetr = side * 4
     square = side ** 2
-    return f'Периметр квадрата - {perimetr},\nПлощадь квадрата - {square}'
+    diagonal = (side ** 2 + side ** 2)**0.5
+    return f'Периметр квадрата - {perimetr},\nПлощадь квадрата - {square},\nДиагональ квадрата - {diagonal}'
 
 result = square(5)
 print(result)
