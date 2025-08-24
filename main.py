acf21c78ad6b61de816244c0da734127d8137a42
g-pale <saburkin.s@outlook.com>
2025-08-24 13:02:59 +0300

    add area square perimetr, square

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
+def square(side):
+    'Функция выводит, периметр и площадь квадрата.'
+    perimetr = side * 4
+    square = side ** 2
+    diagonal = (side ** 2 + side ** 2)**0.5
+    return f'Периметр квадрата - {perimetr},\nПлощадь квадрата - {square}'
 
+<<<<<<< Updated upstream
 
 def area_circle(r):
     s = math.pi * r**2
@@ -8,3 +14,7 @@ def area_circle(r):
 def area_square(a):
     S_s = a**2
     return S_s
+=======
+result = square(5)
+print(result)
+>>>>>>> Stashed changes
